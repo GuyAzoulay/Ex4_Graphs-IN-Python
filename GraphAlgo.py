95ace14838c04448442e995f985fe6726e4882ab
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2022-01-01 17:06:58 +0200

    Add files via upload

--- GraphAlgo.py
+++ GraphAlgo.py
@@ -1,11 +1,11 @@
 import math
-import _tkinter
-from _tkinter import *
-from typing import List
+import time
 
-from IPython.utils.timing import clock
+
 from future.moves import tkinter
 from future.moves.tkinter import simpledialog,messagebox
+import numpy as np
+
 from DiGraph import DiGraph
 from GraphInterface import GraphInterface
 from GraphAlgoInterface import GraphAlgoInterface
@@ -15,17 +15,9 @@ import pygame
 from pygame import Color, display, gfxdraw, font
 from pygame.constants import RESIZABLE
 
-WIDTH, HEIGHT = 1080, 720
-pygame.init()
-screen = display.set_mode((WIDTH, HEIGHT), depth=32, flags=RESIZABLE)
-clock = pygame.time.Clock()
-pygame.font.init()
-
-FONT = pygame.font.SysFont('Arial', 20, bold=True)
-
 def scale(data, min_screen, max_screen, min_data, max_data):
     return int(((data - min_data) / (max_data - min_data)) * (max_screen - min_screen) + min_screen)
-def draw_arrow(color, start, end):
+def draw_arrow(color, start, end,screen):
     rotation = math.degrees(math.atan2(start[1] - end[1], end[0] - start[0])) + 90
     pygame.draw.polygon(screen, color, (
         (end[0] + 10 * math.sin(math.radians(rotation)), end[1] + 10 * math.cos(math.radians(rotation))),
@@ -47,12 +39,15 @@ class GraphAlgo(GraphAlgoInterface):
         try:
             with open(file_name) as f:
                 graph_Loaded = json.load(f)
-            if 'pos' in graph_Loaded['Nodes'][0]:
-                for node in graph_Loaded['Nodes']:
+            #if 'pos' in graph_Loaded['Nodes'][0]:
+            for node in graph_Loaded['Nodes']:
+                if 'pos' in graph_Loaded['Nodes'][0] and node["pos"] != None:
                     self.graph.nodes[node['id']] = tuple((float(n) for n in node["pos"].split(',')))
-            else:
-                for node in graph_Loaded['Nodes']:
-                    self.graph.nodes[node['id']] = None
+                else:
+                    self.graph.nodes[node['id']] = tuple([np.random.uniform(0,40),np.random.uniform(0,40) ,0.0])
+            # else:
+            #     for node in graph_Loaded['Nodes']:
+            #         self.graph.nodes[node['id']] = None
             for edge in graph_Loaded['Edges']:
                 self.graph.edges[(edge['src'], edge['dest'])] = edge['w']
             return True
@@ -126,9 +121,10 @@ class GraphAlgo(GraphAlgoInterface):
 
     def TSP(self, node_lst: list[int]) -> (list[int], float):
         currect_paths = []
+        final_paths = []
         if len(node_lst) == 0:
             return ([], -1)
-        all_Paths = [[]]
+        all_Paths = []
         weight_list = []
         for i in node_lst:
             for j in node_lst:
@@ -137,18 +133,26 @@ class GraphAlgo(GraphAlgoInterface):
                 all_Paths.append(self.shortest_path(i, j)[1])
         for list1 in all_Paths:
             if len(list1) == 0: continue
-            res = list1.__contains__(node_lst)
+            res = any(t in node_lst for t in list1)
             if res:
                 currect_paths.append(list1)
-            else:
-                for list2 in all_Paths:
-                    if len(list2) == 0: continue
-                    if list1[-1] == list2[0] and list1[0] != list2[-1]:
-                        currect_paths.append(list1[0:len(list1) - 1] + list2)
-
+        for list1 in all_Paths:
+            if len(list1) == 0: continue
+            for list2 in all_Paths:
+                if len(list2) == 0: continue
+                if list1[len(list1)-1] == list2[0] and list1[0] != list2[len(list2)-1]:
+                    currect_paths.append(list1[0:len(list1) - 1] + list2)
         for currect in currect_paths:
-            weight_list.append((currect, self.weight_calc(currect)))
-
+            count = 0
+            tmp = []
+            for n in node_lst:
+                tmp.append(n)
+            for c in currect:
+                if tmp.__contains__(c):
+                    count += 1
+                    tmp.remove(c)
+            if count == len(node_lst):
+                weight_list.append((currect, self.weight_calc(currect)))
         return min(weight_list, key=lambda lst: lst[1])
         # the known problem; TSP, Travel SalesMan Problem, here we are given a list of city and we
         # need to find the shortest path which go through all this cities and the weight of this
@@ -163,8 +167,11 @@ class GraphAlgo(GraphAlgoInterface):
 
     def weight_calc(self, list):
         weight = 0
-        for i in list:
-            e = (i, i + 1)
+        counter = list[0]
+        for i in range(len(list)):
+            if i+1 == len(list):
+                break
+            e = (list[i], list[i + 1])
             if e in self.graph.edges.keys():
                 weight += self.graph.edges[e]
             else:
@@ -174,6 +181,7 @@ class GraphAlgo(GraphAlgoInterface):
     # a function that help me to calculate the weight of each list
 
     def centerPoint(self) -> (int, float):
+        a= time.time()
         if not self.isConnected(self.graph):
             return (-1,float('inf'))
         dict = {}
@@ -188,7 +196,8 @@ class GraphAlgo(GraphAlgoInterface):
         key_list = list(dict.keys())
         value_list = list(dict.values())
         t = value_list.index(i)
-
+        b = time.time()
+        print("total time: ",b-a)
         return t, i
 
     # the center function return us the node that is the center of the graph,
@@ -198,6 +207,14 @@ class GraphAlgo(GraphAlgoInterface):
     # of course that in the beginning we check that the graph is strongly connected
 
     def plot_graph(self) -> None:
+
+        WIDTH, HEIGHT = 1080, 720
+        pygame.init()
+        screen = display.set_mode((WIDTH, HEIGHT), depth=32, flags=RESIZABLE)
+        clock = pygame.time.Clock()
+        pygame.font.init()
+
+        FONT = pygame.font.SysFont('Arial', 20, bold=True)
         min_x = min(self.graph.nodes.values(), key=lambda pos_x: pos_x[0])[0]
         min_y = min(self.graph.nodes.values(), key=lambda pos_y: pos_y[1])[1]
         max_x = max(self.graph.nodes.values(), key=lambda pos_x: pos_x[0])[0]
@@ -234,7 +251,7 @@ class GraphAlgo(GraphAlgoInterface):
                 dest_y = scale(self.graph.nodes[e[1]][1], 50, screen.get_height() - 50, min_y, max_y)
                 pygame.draw.line(screen, Color(61, 72, 126),
                                  (src_x, src_y), (dest_x, dest_y), width=2)
-                draw_arrow("black", [src_x, src_y], [(src_x + dest_x) / 2, (src_y + dest_y) / 2])
+                draw_arrow("black", [src_x, src_y], [(src_x + dest_x) / 2, (src_y + dest_y) / 2],screen)
 
             # Drawing the shortest path(tsp or shortestPath)
             for i in range(len(tsp) - 1):
@@ -244,7 +261,7 @@ class GraphAlgo(GraphAlgoInterface):
                 dest_y = scale(self.graph.nodes[tsp[i + 1]][1], 50, screen.get_height() - 50, min_y, max_y)
                 pygame.draw.line(screen, Color(0, 255, 0),
                                  (src_x, src_y), (dest_x, dest_y), width=2)
-                draw_arrow("white", [src_x, src_y], [(src_x + dest_x) / 2, (src_y + dest_y) / 2])
+                draw_arrow("white", [src_x, src_y], [(src_x + dest_x) / 2, (src_y + dest_y) / 2],screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -328,19 +345,40 @@ class GraphAlgo(GraphAlgoInterface):
 
 
 if __name__ == '__main__':
+    g= time.time()
     galgo = GraphAlgo()
-    galgo.load_from_json("A0.json")
-    # galgo.save_to_json("new.json")
+    galgo.load_from_json("T0.json")
+    galgo.plot_graph()
+    # d= time.time()
+    # print("build",d-g)
+    # b = time.time()
+    # print(galgo.centerPoint())
+    # a= time.time()
+    # print("center", a-b)
+    # t= time.time()
+    # galgo.shortest_path(1,12)
+    # f= time.time()
+    # print("shortest" ,f-t)
+    # gg= time.time()
+    # galgo.TSP([0,4,12])
+    # tt= time.time()
+    # print("TSP",tt-gg)
+    #print(galgo.shortest_path(0,524))
+    #print(galgo.TSP([0,7,8]))
+    #print(galgo.TSP([0,7,8]))
+    # galgo.save_to_json("A5.json")
     # for x in a:
     #     print(x)
     # print(galgo.shortest_path(0, 1))
     # print(galgo.isConnected(g))
     # print((galgo.centerPoint()))
     # print(galgo.graph.get_all_v())
-    g = galgo.graph
+    #g = galgo.graph
     #print(galgo.graph)
-    galgo.plot_graph()
+   # galgo.plot_graph()
     #print(galgo.TSP([0,1,6]))
 # print(galgo.centerPoint())
 #  e = g.edges
-# galgo.plot_graph()
+
+    #print(galgo.graph.nodes)
+    #galgo.plot_graph()
